5ef2c897ab3e17def26253db0cadb54196f7967a
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2022-11-30 19:26:41 +0300

    lasson 1.1
    finish on 36 minute

---
+++ les_1/hello.rb
@@ -0,0 +1,48 @@
+puts "Hello world"
+
+a = 1
+print "a = "
+puts a
+
+b = 10
+print "b = "
+puts b
+
+sum = a + b
+print "sum = "
+puts sum
+
+sum += 5
+print "(sum+5) = "
+puts sum
+
+print "5.class = "
+puts 5.class
+
+print "5.class = "
+puts 5.5.class
+
+divide_integer = 10 / 3
+print "divide_integer(10/3) = "
+print  divide_integer
+puts " (do cut off the fractional part (int is int))"
+
+divide_float = 10.0 / 3
+print "divide_float(10.0/3) = "
+print  divide_float
+puts " (if we cut float get float)"
+
+degree_2_2 = 2**2
+print "degree_2_2 = "
+puts degree_2_2
+
+degree_2_3 = 2**3
+print "degree_2_3 = "
+puts degree_2_3
+
+string_var = "somesing "
+print "string_var = "
+puts string_var
+
+print "string_var * 6  = "
+puts string_var*6
