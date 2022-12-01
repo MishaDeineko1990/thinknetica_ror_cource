7ca07b7fa6f5d5430eb9beea8236f4766792e14e
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2022-12-01 22:52:18 +0300

    finish_task_to_part_1_base_ruby_on_railse

--- Lesson 1/3.rb
+++ Lesson 1/3.rb
@@ -4,9 +4,9 @@ language = (gets.chomp.to_i)-1
 
 program_name = ["A right triangle?", "Треугольник прямоугольный?","Трикутник прямокутний?"]
 question = [
-    ["The size of the", " side of the triangle"],
-    ["Розмер", " стороны триугольника"],
-    ["Розмір"," сторони трикутника"]
+    ["The size of the","side of the triangle"],
+    ["Розмер", "стороны триугольника"],
+    ["Розмір","сторони трикутника"]
 ]
 number_side_triangle = [
     ["first", "second", "third"],
@@ -14,17 +14,31 @@ number_side_triangle = [
     ["першої", "другої", "третьої"]
 ]
 
+side_triangle = []
 
 puts "-------#{program_name[language]}-------"
 puts ""
 
-# 3.times
-print "#{question_1[language]}? "
-height_triangle = gets.chomp.to_i
+3.times do |i|
+    print "#{question[language][0]} #{number_side_triangle[language][i]} #{question[language][1]}? "
+    side_triangle.push(gets.chomp.to_i)
+end
 
+side_triangle.sort!
+side_triangle.reverse!
 
-
-answer_num = height_triangle*leanth_base_triangle*0.5
-
-puts"#{program_name[language]} = #{answer_num}}"
+puts side_triangle
+ 
+if side_triangle[0]**2 == side_triangle[1]**2 + side_triangle[2]**2
+    puts "#{program_name[language[-1]]}!"
+else    
+    case language
+        when 0  
+          puts "triangle not a right!"         
+        when 1  
+          puts "Триугольник не прямоугольный!"         
+        when 2  
+          puts "Трикутник не прямокутний!"
+    end
+end
 
---
+++ Lesson 1/4.rb
@@ -0,0 +1,21 @@
+print "enter a = "
+a = gets.chomp.to_i
+puts ""
+
+print "enter b = "
+b = gets.chomp.to_i
+puts ""
+
+print "enter c = "
+c = gets.chomp.to_i
+puts ""
+
+d = b**2-4*a*c
+
+if d<0
+    puts "not sqrt"
+    exit
+end
+
+puts "x1 = #{((b * -1) + Math.sqrt(d))/(2*a)}"
+puts "x1 = #{((b * -1) - Math.sqrt(d))/(2*a)}"
