497dee7fddfa59313333928254e68a626b91902a
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-02-03 02:52:30 +0300

    7.3

--- lesson_7/passenger_wagon.rb
+++ lesson_7/passenger_wagon.rb
@@ -1,11 +1,13 @@
 require_relative 'wagon.rb'
 
 class PassengerWagon < Wagon
-  attr_reader :type
   
-  def initialize
-    super
-    @type = "passenger"  
+  def initialize(count_size)
+    super(sids_size, "passenger")      
+  end
+
+  def fill
+    @filled += 1
   end
   
 end  
--- lesson_7/wagon.rb
+++ lesson_7/wagon.rb
@@ -2,4 +2,11 @@ require_relative 'manufacturer.rb'
 
 class Wagon
   include Manufacturer::InstanceMethods
+  attr_reader :type, :filled
+
+  def initialize (volume, type)
+    @type = type
+    @filled = 0
+    @full_volume = volume
+  end
 end  
