5d8e76d2e3879718c2cc61f53e744ee8ccda05c4
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-02-17 02:28:11 +0300

    Update 9.rb

--- my_folder/lesson_9 Метопрограмирование/9.rb
+++ my_folder/lesson_9 Метопрограмирование/9.rb
@@ -28,4 +28,41 @@ loop do
   end
 end
 
+class Foo
+  @@y = 12
+  def initialize
+    @bar = "initialize"
+  end
+
+  private 
+
+  def privat_method
+    puts "privat method"
+  end  
+end
+
+foo_1 = Foo.new
+
+foo_1.instance_eval('privat_method')
+
+foo_1.instance_eval do 
+  def m
+    puts "init variable #{@bar}"
+    privat_method
+  end
+  end
+
+foo_1.m
+
+Foo.class_evall("@@y")
+
+Foo.class_evall do #додаємо метод оєкта класса
+  def m_2
+    puts "file m"
+  end
+end
+
+foo_2 = Foo.new
+
+foo_2.m_2
 
